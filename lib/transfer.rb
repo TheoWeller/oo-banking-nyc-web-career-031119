ac19b19235a2b47483dcf3d0112e825da5495197
Theo Weller <weller.theodor@gmail.com>
2019-03-14 16:55:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+  def initialize(sender, receiver, amount)
+@sender = sender
+@receiver = receiver
+@status = 'pending'
+@amount = amount
+  end
+
+  def valid?
+    sender.valid?
+    receiver.valid?
+  end
+
+def execute_transaction
+  if !sender.valid? || sender.balance < amount
+    @status = 'rejected'
+    "Transaction rejected. Please check your account balance."
+  elsif @status == 'pending'
+    sender.balance -= amount
+    receiver.balance += amount
+    return @status = 'complete'
+  end
+end
+
+def reverse_transfer
+    if @status == 'complete'
+sender.balance += amount
+receiver.balance -= amount
+    return @status == 'reversed'
+    end
+  end
 end
